deabfa46b5e5363903861158ff7a73f0de728bb6
Varun Gupta <varungupta2015135@gmail.com>
2019-09-26 20:56:00 +0530

    Initial Commit

---
+++ src/main/java/todo/JustDoItApplication.java
@@ -0,0 +1,15 @@
+package todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.Console;
+
+@SpringBootApplication
+public class JustDoItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustDoItApplication.class, args);
+	}
+
+}
