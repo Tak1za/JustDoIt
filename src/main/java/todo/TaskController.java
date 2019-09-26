2492f44ae85d24e208cf6c0758fccc676cfb5ec9
Varun Gupta <varungupta2015135@gmail.com>
2019-09-26 22:53:06 +0530

    Added cross origin resources

--- src/main/java/todo/TaskController.java
+++ src/main/java/todo/TaskController.java
@@ -1,11 +1,13 @@
 package todo;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:4200"})
 @RestController
 public class TaskController {
     private final TaskRepository repository;
